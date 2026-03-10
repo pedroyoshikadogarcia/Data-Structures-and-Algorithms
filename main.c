ce961c7fac616f01244c22212963ad61bce6e54a
Pedro <pedroyoshikado@gmail.com>
2026-03-10 11:16:52 -0300

    Setup inicial do projeto e configuracao do gitignore

---
+++ main.c
@@ -0,0 +1,11 @@
+//Bibliotecas 
+#include<stdio.h> //O <> é pq ja existe uma biblioteca se fosse criado por mim seria ""
+//Variaveis
+
+//Programa
+int main(){
+    printf("Iai mundo :)");
+
+    return 0;
+}
+
